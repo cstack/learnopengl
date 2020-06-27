73f242354bc1472dd095ef3ddd89b99fe6ff7e76
Connor Stack <sirconnorstack@gmail.com>
2020-06-26 21:16:00 -0700

    Encapsulate initialization. Close window on ESC key.

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,43 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
-void framebuffer_size_callback(GLFWwindow*, int, int);
+void        framebuffer_size_callback(GLFWwindow*, int, int);
+GLFWwindow* initialize();
+void        processInput(GLFWwindow*);
 
 int main() {
+  GLFWwindow* window = initialize();
+  if (window == NULL) {
+    return -1;
+  }
+  printf("Successfully initialized GLFW and GLAD\n");
+
+  // Render loop
+  // Continue until GLFW instructed to close
+  // (if user exits window)
+  while(!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    // We are done rendering to back buffer.
+    // Swap with front buffer to update frame
+    // instantaneously. The technique is called:
+    // Double Buffer
+    glfwSwapBuffers(window);
+
+    // Check for events (keyboard, mouse, window size)
+    // Update window state, then do callbacks
+    glfwPollEvents();
+  }
+
+  printf("GLFW has been instructed to close\n");
+
+  // clean up all GLFW resources that were allocated
+  glfwTerminate();
+
+  return 0;
+}
+
+GLFWwindow* initialize() {
   glfwInit(); // GLFW handles windows, user input
 
   // glfwWindowHint docs:
@@ -32,7 +66,7 @@ int main() {
   if (window == NULL) {
     printf("Failed to create GLFW window\n");
     glfwTerminate();
-    return -1;
+    return NULL;
   }
 
   glfwMakeContextCurrent(window);
@@ -40,7 +74,7 @@ int main() {
   // Tell GLAD how to look up functions
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     printf("Failed to initialize GLAD\n");
-    return -1;
+    return NULL;
   }
 
   glViewport(
@@ -53,32 +87,17 @@ int main() {
   // Window resize callback
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  printf("Hello World\n");
-
-  // Render loop
-  // Continue until GLFW instructed to close
-  // (if user exits window)
-  while(!glfwWindowShouldClose(window)) {
-    // We are done rendering to back buffer.
-    // Swap with front buffer to update frame
-    // instantaneously. The technique is called:
-    // Double Buffer
-    glfwSwapBuffers(window);
-
-    // Check for events (keyboard, mouse, window size)
-    // Update window state, then do callbacks
-    glfwPollEvents();
-  }
-
-  printf("GLFW has been instructed to close\n");
-
-  // clean up all GLFW resources that were allocated
-  glfwTerminate();
-
-  return 0;
+  return window;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   printf("framebuffer_size_callback(%d, %d)\n", width, height);
   glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    printf("Pressed ESC. Will close.\n");
+    glfwSetWindowShouldClose(window, true);
+  }
 }
